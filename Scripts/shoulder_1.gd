5d6f179256631f79e5f6001c0ae00e176a33846a
Anastashia Fongrad <fongradnastya@gmail.com>
2022-03-24 10:07:04 +0700

    Added robot sprite

---
+++ Scripts/shoulder_1.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+
+var velocity = Vector2()
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("one"):
+		get_input()
+		velocity = rotate(0.1)
