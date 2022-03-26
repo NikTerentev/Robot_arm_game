c483f8076e2a4ab8b61509b564d940eaa2679943
Anastashia Fongrad <fongradnastya@gmail.com>
2022-03-26 17:28:45 +0700

    Start adding collisions

---
+++ Scenes/Ground.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	# print(position, get_local_mouse_position())
+	pass
