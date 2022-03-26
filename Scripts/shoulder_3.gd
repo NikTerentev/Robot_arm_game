f9a788b00bdaf0e9002cf47f484bd72c7a1b9ffb
Anastashia Fongrad <fongradnastya@gmail.com>
2022-03-26 18:32:16 +0700

    New bacground and groups

---
+++ Scripts/shoulder_3.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+
+var velocity = Vector2()
+var pos = Vector2()
+
+func get_input():
+	look_at(pos)
+	print(pos.y, position)
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("three"):
+		pos = get_global_mouse_position()
+		if pos.y < 700:
+			get_input()
+			velocity = apply_torque_impulse(0.1)
+		else:
+			print("Wrong", pos.y)
