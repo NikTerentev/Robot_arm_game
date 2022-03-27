0f0344ff91975827af0a0f510f2cfe93ac915683
Anastashia Fongrad <fongradnastya@gmail.com>
2022-03-27 18:41:08 +0700

    Added levels map

---
+++ Scripts/Levels.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+var three_stars = load("res://art/scene3/3 stars.png")
+var two_stars = load("res://art/scene3/2_ stars.png")
+var one_star = load("res://art/scene3/star.png")
+
+func set_star(level, num):
+	if num == 1:
+		level.texture = one_star
+	elif num == 2:
+		level.texture = two_stars
+	else:
+		level.texture = three_stars
+
+
+func _ready():
+	set_star($group/stars_1, StarsForLevels.stars[0])
+	set_star($group/stars_2, StarsForLevels.stars[1])
+	set_star($group/stars_3, StarsForLevels.stars[2])
+	set_star($group/stars_4, StarsForLevels.stars[3])
+	set_star($group/stars_5, StarsForLevels.stars[4])
+	set_star($group/stars_6, StarsForLevels.stars[5])
+	set_star($group/stars_7, StarsForLevels.stars[6])
+
+
+func _process(delta):
+	pass
+
+
+func _on_back_pressed():
+	get_tree().change_scene("res://GameMode.tscn")
