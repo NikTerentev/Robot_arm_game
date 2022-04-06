a1a099925350dee093382d5a136b93ef27dde33a
Anastashia Fongrad <fongradnastya@gmail.com>
2022-04-06 16:52:05 +0700

    Edded end scene

--- Scripts/Level_7/MainScene.gd
+++ Scripts/Level_7/MainScene.gd
@@ -2,28 +2,31 @@ extends Node2D
 
 var is_stand_1 = 0
 var is_stand_2 = 0
+var wheels = []
 
 func _ready():
-	$wheel.apply_central_impulse(Vector2(500, 0))
+	wheels = get_tree().get_nodes_in_group("wheel")
 
 
 func _on_TextureButton_pressed():
-	print("Нажали!")
+	pass
 
 
 func _on_placeholder_1_body_entered(body):
-	if not is_stand_1:
-		print(body)
+	if not is_stand_1 and body in wheels:
 		$picture.show()
 		body.hide()
 		is_stand_1 = 1
 		
 
+
 func _on_placeholder2_body_entered(body):
-	if not is_stand_2:
-		print(body)
+	if not is_stand_2 and body in wheels:
 		$picture2.show()
 		body.hide()
 		is_stand_2 = 1
 	
 
+func _process(delta):
+	if is_stand_1 and is_stand_2:
+		get_tree().change_scene("res://Scenes/Menu/Winning.tscn")
