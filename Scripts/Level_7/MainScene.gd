c9e9d0a5604bfc9e43679f48e29c1f71c23dfe44
Anastashia Fongrad <fongradnastya@gmail.com>
2022-04-06 16:21:50 +0700

    Добавлена установка колеса

--- Scripts/Level_7/MainScene.gd
+++ Scripts/Level_7/MainScene.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
+var is_stand_1 = 0
+var is_stand_2 = 0
 
 func _ready():
-	pass
+	$wheel.apply_central_impulse(Vector2(500, 0))
 
 
 func _on_TextureButton_pressed():
 	print("Нажали!")
+
+
+func _on_placeholder_1_body_entered(body):
+	if not is_stand_1:
+		print(body)
+		$picture.show()
+		body.hide()
+		is_stand_1 = 1
+		
+
+func _on_placeholder2_body_entered(body):
+	if not is_stand_2:
+		print(body)
+		$picture2.show()
+		body.hide()
+		is_stand_2 = 1
+	
+
