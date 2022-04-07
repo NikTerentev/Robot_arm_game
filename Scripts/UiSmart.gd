8d45d95864a9146d46ac1cbd790d29a73ee9f79d
Terentev Nikita <nitro-2003@mail.ru>
2022-04-07 18:40:55 +0700

    Fixed game

--- Scripts/UiSmart.gd
+++ Scripts/UiSmart.gd
@@ -1,20 +1,8 @@
 extends CanvasLayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	BackgroundMusic.stop()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Back_pressed():
 	$ClickSound.play()
