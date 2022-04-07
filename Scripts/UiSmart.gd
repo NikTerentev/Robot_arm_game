df63b64e9fac62be4b6bda989477ed40d67492d1
fongradnastya <fongradnastya@gmail.com>
2022-04-07 13:14:28 +0700

    Pause added

--- Scripts/UiSmart.gd
+++ Scripts/UiSmart.gd
@@ -1,17 +1,19 @@
 extends CanvasLayer
 
 var time = 0
+const wait_time = 20 
 func _ready():
 	$Timer.start()
 	BackgroundMusic.stop()
 
 
 func _process(delta):
-	time += delta
-	var sec = fmod(time, 60)
-	var mins = fmod(time, 3600) / 60
-	var time_str = "%02d:%02d" % [mins, sec]
-	$time_counter/Label.text = time_str
+	if not $Timer.is_stopped():
+		time += delta
+		var sec = fmod(time, 60)
+		var mins = fmod(time, 3600) / 60
+		var time_str = "%02d:%02d" % [mins, sec]
+		$time_counter/Label.text = time_str
 	
 func _on_Back_bt_pressed():
 	print("pessed")
@@ -22,3 +24,11 @@ func _on_Back_bt_pressed():
 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://Scenes/Menu/Losing.tscn")
+
+func _on_pause_pressed():
+	if $Timer.is_stopped():
+		$Timer.wait_time = wait_time - fmod(time, 60)
+		$Timer.start()
+		
+	else:
+		$Timer.stop()
