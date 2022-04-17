15cb1a094e4e487e183c3bc56ffe82922b2a84b7
Terentev Nikita <nitro-2003@mail.ru>
2022-04-17 18:44:43 +0700

    task #25 Improved button textures

--- Scripts/Menu/Losing.gd
+++ Scripts/Menu/Losing.gd
@@ -6,8 +6,12 @@ func _ready():
 
 
 func _on_go_on_pressed():
-	get_tree().change_scene("res://Scenes/Menu/UiSmart.tscn")
+	$ClickSound.play()
+	yield($ClickSound, "finished")
+	get_tree().change_scene("res://Scenes/Level7/CarAssembly.tscn")
 
 
 func _on_back_pressed():
+	$ClickSound.play()
+	yield($ClickSound, "finished")
 	get_tree().change_scene("res://Scenes/Menu/MainMenu.tscn")
